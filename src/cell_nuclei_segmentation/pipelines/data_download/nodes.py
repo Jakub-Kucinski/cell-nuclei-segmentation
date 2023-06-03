4ef84860d5d100d4f88c329f053888e95c674dc9
Jakub-Kucinski <jakub.kucinski21@gmail.com>
2023-06-03 14:38:30 +0000

    Add data_download pipeline and nodes

---
+++ src/cell_nuclei_segmentation/pipelines/data_download/nodes.py
@@ -0,0 +1,9 @@
+def dummy_download() -> set:
+    """Function performing dummy download.
+    (Returns empty set and lets dataset implementation download the data)
+
+    Returns:
+        set: Empty set imitating downloaded dataset.
+    """
+    data = {}
+    return data
